fdf73be7c4a3d50929e3024a8b31b8a1d92e4f4b
Ivan <strateg8888@gmail.com>
2022-10-11 22:45:35 +0400

    Registration form test 3hw

---
+++ src/test/java/com/demoqa/TestDemoRegForm.java
@@ -0,0 +1,61 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestDemoRegForm {
+    @BeforeAll
+    static void beforeSettings() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void testRegForm() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Muratov");
+        $("#userEmail").setValue("examplemail@mail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("8005553535");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("1995");
+        $(".react-datepicker__day--027").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("11.jpg");
+
+        $("#currentAddress").setValue("Address, Street, Building");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        $(".table-responsive").shouldHave(text("Ivan Muratov"),
+                text("examplemail@mail.com"),
+                text("Male"),
+                text("8005553535"),
+                text("27 May,1995"),
+                text("Computer Science"),
+                text("Sports"),
+                text("11.jpg"),
+                text("Address, Street, Building"),
+                text("NCR Delhi"));
+    }
+}
